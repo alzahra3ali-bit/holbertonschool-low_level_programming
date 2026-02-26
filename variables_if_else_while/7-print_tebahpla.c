995c96e0da59fbab646dd02d988138ea9635e36e
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-02-26 20:51:01 +0000

    Task 77

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -8,10 +8,11 @@
 int main(void)
 {
 	char c;
-	for(c = 'z'; c <= 'z'; c--)
-	{
-      putchar(c);
-	}
-putchar('\n');
+
+	for (c = 'z'; c >= 'a'; c--)
+		putchar(c);
+
+	putchar('\n');
+
 	return (0);
 }
