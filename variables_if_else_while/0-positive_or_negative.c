d8d495eab8f9db3782ab1c54070d398fa0271bb0
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-02-26 19:36:12 +0000

    Task 0000

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -14,15 +14,15 @@ int main(void)
     n = rand() - RAND_MAX / 2;
 	if(n>0)
 	{
-	printf("%dis positive\n",n);
+	printf("%dis positive \n",n);
 	}
 	else if(n==0)
 	{
-	printf("%dis zero\n",n);
+	printf("%dis zero \n",n);
 	}
 	else
 	{
-        printf("%dis negative\n",n);
+        printf("%dis negative \n",n);
 	}
 	return (0);
 }
