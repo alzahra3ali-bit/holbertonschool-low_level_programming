e27b040363316f23dcf389d611bfb35044e0ae2e
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-03-03 20:24:43 +0000

    Task 1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -2,20 +2,20 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-    int i = 0;
-    int j = 0;
+	int i = 0;
+	int j = 0;
 
-    while (dest[i] != '\0')
-        i++;
+	while (dest[i] != '\0')
+		i++;
 
-    while (j < n && src[j] != '\0')
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-    dest[i] = '\0';
+	dest[i] = '\0';
 
-    return dest;
+	return (dest);
 }
