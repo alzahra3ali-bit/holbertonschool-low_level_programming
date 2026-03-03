b2cfda1989a3a8acb989b03524688a718974157d
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-03-03 19:38:41 +0000

    Task 10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: the string to convert
+ *
+ * Return: the integer value
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int sign = 1;
+	int result = 0;
+	int started = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			started = 1;
+			result = result * 10 + (s[i] - '0');
+		}
+		else if (started == 1)
+			break;
+
+		i++;
+	}
+
+	return (result * sign);
+}
