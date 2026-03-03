49b6744593908a0841c4a1ff52865c4a206711fb
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-03-03 19:09:17 +0000

    Task 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -2,19 +2,16 @@
 
 /**
  * puts2 - prints every other character of a string
- * @str: string to print
- *
- * Return: nothing
+ * @str: input string
  */
 void puts2(char *str)
 {
-	int i = 0;
+    int i;
 
-	while (str[i] != '\0' && str[i] >= 32 && str[i] <= 126)
-	{
-		_putchar(str[i]);
-		i += 2;
-	}
+    for (i = 0; str[i] != '\0'; i += 2)
+    {
+        _putchar(str[i]);
+    }
 
-	_putchar('\n');
+    _putchar('\n');
 }
