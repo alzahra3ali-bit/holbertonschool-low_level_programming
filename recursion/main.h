9393c48d2846b707f59380de804fb0f71c892527
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-03-11 17:59:59 +0000

    Task 0

--- recursion/main.h
+++ recursion/main.h
@@ -1,10 +1,7 @@
-/**
- * main - check the code
- * 
- * Return: Always 0.
- */
-int main(void)
-{
-    _puts_recursion("Puts with recursion");
-    return (0);
-}
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif
