f85f8fc3953ae9305c2bc846bbcd311280177240
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-04-03 10:09:25 +0000

    Task 0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: pointer to allocated memory
+ *         if malloc fails, terminates process with status 98
+ */
+void *malloc_checked(unsigned int b)
+{
+    void *ptr;
+
+    ptr = malloc(b);          // حاول حجز الذاكرة
+    if (ptr == NULL)          // لو malloc فشل
+        exit(98);             // انهي البرنامج مع الحالة 98
+
+    return ptr;               // رجع المؤشر للذاكرة
+}
