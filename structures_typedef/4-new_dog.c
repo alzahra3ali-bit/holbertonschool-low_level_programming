8877e221cd53fd52fb0288a3bd17587ca929ce4f
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-04-04 17:29:11 +0000

    Task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,11 +2,23 @@
 #include "dog.h"
 
 /**
+ * str_len - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+int str_len(char *s)
+{
+    int i = 0;
+    while (s[i])
+        i++;
+    return i;
+}
+
+/**
  * new_dog - creates a new dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
- * 
  * Return: pointer to new dog, or NULL if it fails
  */
 dog_t *new_dog(char *name, float age, char *owner)
@@ -21,7 +33,7 @@ dog_t *new_dog(char *name, float age, char *owner)
     if (!d)
         return NULL;
 
-    d->name = malloc(strlen(name) + 1);
+    d->name = malloc(str_len(name) + 1);
     if (!d->name)
     {
         free(d);
@@ -31,7 +43,7 @@ dog_t *new_dog(char *name, float age, char *owner)
         d->name[i] = name[i];
     d->name[i] = '\0';
 
-    d->owner = malloc(strlen(owner) + 1);
+    d->owner = malloc(str_len(owner) + 1);
     if (!d->owner)
     {
         free(d->name);
