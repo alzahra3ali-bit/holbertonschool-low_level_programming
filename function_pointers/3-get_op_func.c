4c4aa21c72c4ea23ed809b3a957d395c46bd670d
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-04-10 15:41:23 +0000

    Task 3

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - selects correct function
+ * @s: operator
+ *
+ * Return: pointer to function
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	int i = 0;
+
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	while (ops[i].op != NULL && s[0] != ops[i].op[0])
+		i++;
+
+	if (ops[i].op != NULL)
+		return (ops[i].f);
+
+	return (NULL);
+}
