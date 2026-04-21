f91b8263c8dd0aab31534ed25ed32b3730a5f6f9
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-04-21 12:55:27 +0000

    Task 1

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -9,10 +9,11 @@
 
 size_t list_len(const list_t *h)
 {
-  int count = 0;
-   while ( h != NULL) {
-    count++;
-    h = h->next;
-   }
+int count = 0;
+while (h != NULL)
+{
+count++;
+h = h->next;
+}
  return (count);
 }
