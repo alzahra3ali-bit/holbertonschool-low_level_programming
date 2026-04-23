3d13f7f37f452d3858a61dd98248154f72ec474b
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-04-23 14:36:03 +0000

    Task0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -12,42 +12,42 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t r_bytes, w_bytes;
-    char *buffer;
+	int fd;
+	ssize_t r_bytes, w_bytes;
+	char *buffer;
 
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-    {
-        close(fd);
-        return (0);
-    }
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
 
-    r_bytes = read(fd, buffer, letters);
-    if (r_bytes == -1)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	r_bytes = read(fd, buffer, letters);
+	if (r_bytes == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-    w_bytes = write(STDOUT_FILENO, buffer, r_bytes);
-    if (w_bytes == -1 || w_bytes != r_bytes)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	w_bytes = write(STDOUT_FILENO, buffer, r_bytes);
+	if (w_bytes == -1 || w_bytes != r_bytes)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-    free(buffer);
-    close(fd);
+	free(buffer);
+	close(fd);
 
-    return (r_bytes);
+	return (r_bytes);
 }
