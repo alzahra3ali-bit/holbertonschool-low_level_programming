fa65063d664dd21aa2471057f12b6281e618b56e
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-04-23 12:54:03 +0000

    Task 5

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -3,19 +3,21 @@
 
 /**
  *get_dnodeint_at_index - return the nth node
- * @index : it is the index for the node we want to get
+ * @index: it is the index for the node we want to get
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-dlistint_t temp = head;
+dlistint_t *temp = head;
+int i = 0;
 while (temp != NULL)
 {
-if (temp->index == index )
+if (i == index )
 {
 return (temp);
 }
 else
 temp =  temp->next;
+i++;
 }
 return (NULL);
 }
