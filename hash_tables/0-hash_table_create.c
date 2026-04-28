9e692141519d4f1e347a8d2d6b7f60476607e5ae
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-04-28 13:46:41 +0000

    Task 0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,15 +1,16 @@
 #include "hash_tables.h"
-/*
+/**
  *hash_table_t - method creat a hash table
  *@size : is the size of hash table
+ *
  *Return : the new hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t*  MyTable;
 unsigned long int i;
-hash_node_t** array;
-MyTable = malloc(sizeof(hash_table_t));
+hash_node_t **array;
+MyTable = malloc (sizeof(hash_table_t));
 if (MyTable == NULL)
 {
 return(NULL);
@@ -22,7 +23,7 @@ free(MyTable);
 return(NULL);
 }
 MyTable->array = array;
-for(i = 0; i< size; i++)
+for (i = 0; i < size; i++)
 {
 array[i] = NULL;
 }
