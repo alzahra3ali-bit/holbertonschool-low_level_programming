b7c905791a0330be840ebb52c75de1539c8ce8cd
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-04-28 13:28:15 +0000

    Task 0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,3 +1,9 @@
+#includ "main.h"
+/*
+ *hash_table_t - method creat a hash table
+ *@size : is the size of hash table
+ *Return : the new hash table
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t*  MyTable;
