8b2a73fa8b329b0aa272a1ee31e7f96a3a4794e9
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-04-28 13:48:16 +0000

    Task00

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,31 +1,34 @@
 #include "hash_tables.h"
+
 /**
- *hash_table_t - method creat a hash table
- *@size : is the size of hash table
+ * hash_table_create - creates a hash table
+ * @size: size of the hash table
  *
- *Return : the new hash table
+ * Return: pointer to the new hash table, or NULL if failed
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t*  MyTable;
-unsigned long int i;
-hash_node_t **array;
-MyTable = malloc (sizeof(hash_table_t));
-if (MyTable == NULL)
-{
-return(NULL);
-}
-MyTable->size = size;
-array = malloc(sizeof(hash_node_t *)*size);
-if (array == NULL)
-{
-free(MyTable);
-return(NULL);
-}
-MyTable->array = array;
-for (i = 0; i < size; i++)
-{
-array[i] = NULL;
-}
-return(MyTable);
+    hash_table_t *MyTable;
+    hash_node_t **array;
+    unsigned long int i;
+
+    MyTable = malloc(sizeof(hash_table_t));
+    if (MyTable == NULL)
+        return (NULL);
+
+    MyTable->size = size;
+
+    array = malloc(sizeof(hash_node_t *) * size);
+    if (array == NULL)
+    {
+        free(MyTable);
+        return (NULL);
+    }
+
+    MyTable->array = array;
+
+    for (i = 0; i < size; i++)
+        MyTable->array[i] = NULL;
+
+    return (MyTable);
 }
