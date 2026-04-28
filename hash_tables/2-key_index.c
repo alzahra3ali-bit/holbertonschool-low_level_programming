3196d336808356e2903ff103ec16f40e39914162
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-04-28 14:10:25 +0000

    Task 2

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -9,7 +9,7 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int hash_value;
-	hash_value = hash_djb2(key);	
-	return (hash_value % size);
+unsigned long int hash_value;
+hash_value = hash_djb2(key);	
+return (hash_value % size);
 }
