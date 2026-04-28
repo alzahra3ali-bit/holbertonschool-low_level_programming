5512f4f89a32a5c12ae0575b10a9ece9d2694526
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-04-28 13:49:49 +0000

    Task 0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -8,27 +8,27 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *MyTable;
-    hash_node_t **array;
-    unsigned long int i;
+	hash_table_t *MyTable;
+	hash_node_t **array;
+	unsigned long int i;
 
-    MyTable = malloc(sizeof(hash_table_t));
-    if (MyTable == NULL)
-        return (NULL);
+	MyTable = malloc(sizeof(hash_table_t));
+	if (MyTable == NULL)
+		return (NULL);
 
-    MyTable->size = size;
+	MyTable->size = size;
 
-    array = malloc(sizeof(hash_node_t *) * size);
-    if (array == NULL)
-    {
-        free(MyTable);
-        return (NULL);
-    }
+	array = malloc(sizeof(hash_node_t *) * size);
+	if (array == NULL)
+	{
+		free(MyTable);
+		return (NULL);
+	}
 
-    MyTable->array = array;
+	MyTable->array = array;
 
-    for (i = 0; i < size; i++)
-        MyTable->array[i] = NULL;
+	for (i = 0; i < size; i++)
+		MyTable->array[i] = NULL;
 
-    return (MyTable);
+	return (MyTable);
 }
