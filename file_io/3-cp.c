aff23e14f3fa4c49bce948fd34a396437735ad96
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-05-17 14:22:37 +0000

    Task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -19,6 +19,41 @@ void close_fd(int fd)
 }
 
 /**
+ * copy_file_contents - Reads from source and writes to destination.
+ * @fd_from: File descriptor of the source file.
+ * @fd_to: File descriptor of the destination file.
+ * @file_from: Name of the source file.
+ * @file_to: Name of the destination file.
+ *
+ * Return: void.
+ */
+void copy_file_contents(int fd_from, int fd_to, char *file_from, char *file_to)
+{
+	ssize_t n_read, n_written;
+	char buffer[1024];
+
+	while ((n_read = read(fd_from, buffer, 1024)) > 0)
+	{
+		n_written = write(fd_to, buffer, n_read);
+		if (n_written != n_read)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			close_fd(fd_from);
+			close_fd(fd_to);
+			exit(99);
+		}
+	}
+
+	if (n_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		close_fd(fd_from);
+		close_fd(fd_to);
+		exit(98);
+	}
+}
+
+/**
  * main - Copies the content of a file to another file.
  * @argc: The number of arguments passed to the program.
  * @argv: An array of pointers to the arguments.
@@ -28,8 +63,6 @@ void close_fd(int fd)
 int main(int argc, char *argv[])
 {
 	int fd_from, fd_to;
-	ssize_t n_read, n_written;
-	char buffer[1024];
 
 	if (argc != 3)
 	{
@@ -52,25 +85,7 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	while ((n_read = read(fd_from, buffer, 1024)) > 0)
-	{
-		n_written = write(fd_to, buffer, n_read);
-		if (n_written != n_read)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close_fd(fd_from);
-			close_fd(fd_to);
-			exit(99);
-		}
-	}
-
-	if (n_read == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close_fd(fd_from);
-		close_fd(fd_to);
-		exit(98);
-	}
+	copy_file_contents(fd_from, fd_to, argv[1], argv[2]);
 
 	close_fd(fd_from);
 	close_fd(fd_to);
