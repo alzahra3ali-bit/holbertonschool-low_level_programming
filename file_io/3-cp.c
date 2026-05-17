f8c3d22cc731dcac6b89e2bb6b91b9fe989b616c
alzahra3ali-bit <alzahra.3ali@gmail.com>
2026-05-17 14:18:46 +0000

    Task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -6,18 +6,59 @@
 
 #define BUFFER_SIZE 1024
 
-void close_fd(int fd);
-void print_usage(void);
-void read_error(char *file);
-void write_error(char *file);
-void copy_file(int fd_from, int fd_to, char *file_from, char *file_to);
+void print_usage(void)
+{
+	dprintf(STDERR_FILENO,
+		"Usage: cp file_from file_to\n");
+	exit(97);
+}
+
+void read_error(char *file)
+{
+	dprintf(STDERR_FILENO,
+		"Error: Can't read from file %s\n", file);
+	exit(98);
+}
+
+void write_error(char *file)
+{
+	dprintf(STDERR_FILENO,
+		"Error: Can't write to %s\n", file);
+	exit(99);
+}
+
+void close_fd(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+void copy_file(int fd_from, int fd_to, char *file_from, char *file_to)
+{
+	char buffer[BUFFER_SIZE];
+	int rd, wr;
+
+	rd = read(fd_from, buffer, BUFFER_SIZE);
+
+	while (rd > 0)
+	{
+		wr = write(fd_to, buffer, rd);
+		if (wr == -1)
+			write_error(file_to);
+
+		rd = read(fd_from, buffer, BUFFER_SIZE);
+	}
+
+	if (rd == -1)
+		read_error(file_from);
+}
 
 /**
- * main - entry point
- * @argc: arguments count
- * @argv: arguments vector
- *
- * Return: 0 on success
+ * main - copies file content
  */
 
 int main(int argc, char *argv[])
